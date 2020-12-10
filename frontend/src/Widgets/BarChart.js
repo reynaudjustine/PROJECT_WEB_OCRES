df4c0453ee8a7ea4a592b8e870cd4faa43feb109
Justine <justine.reynaud@edu.ece.fr>
2020-12-11 00:00:27 +0100

    Barchart fini

--- frontend/src/Widgets/BarChart.js
+++ frontend/src/Widgets/BarChart.js
@@ -29,9 +29,36 @@ axios.get('http://localhost:3000/shoes/marque/Nike')
 let SnNike=0;
 let SnAd=0;
 let SnVans=0;
-let SnPumas=0;
+let SnPuma=0;
 let SnConv=0;
 
+//Sport 
+let SpNike=0;
+let SpAd=0;
+let SpAsics=0;
+let SpPuma=0;
+let SpSal=0;
+
+//Plage
+let PNike=0;
+let PFila=0;
+let PHav=0;
+let PIpa=0;
+let PKappa=0;
+
+//Ville
+let VBex=0;
+let VGren=0;
+let VCel=0;
+let VChloe=0;
+let VBerl=0;
+
+//Bottines
+let BDoc=0;
+let BUgg=0;
+let BMoon=0;
+let BTimb=0;
+let BMin=0;
 
 
 var categorie = "Sneakers";
@@ -78,7 +105,7 @@ export default class Example extends PureComponent {
        SnNike: 0,
        SnAd: 0,
        SnVans: 0,
-       SnPumas: 0,
+       SnPuma: 0,
        SnConv: 0,
    };
 
@@ -87,6 +114,8 @@ export default class Example extends PureComponent {
 
  componentDidMount()
  {
+
+  //Sneakers
    axios.get('http://localhost:3000/shoes/categorie/Sneakers/Nike')
    .then(res => {
      SnNike=res.data.length;
@@ -106,15 +135,154 @@ export default class Example extends PureComponent {
      this.setState({SnVans})
    })
 
+   axios.get('http://localhost:3000/shoes/categorie/Sneakers/Puma')
+   .then(res => {
+     SnPuma=res.data.length;
+     this.setState({SnPuma})
+   })
+
    axios.get('http://localhost:3000/shoes/categorie/Sneakers/Converse')
    .then(res => {
      SnConv=res.data.length;
      this.setState({SnConv})
    })
+
+    //************************************************** 
+    //Sport 
+
+    axios.get('http://localhost:3000/shoes/categorie/Sport/Nike')
+    .then(res => {
+      SpNike=res.data.length;
+      this.setState({SpNike})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Sport/Adidas')
+    .then(res => {
+      SpAd=res.data.length;
+      this.setState({SpAd})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Sport/Asics')
+    .then(res => {
+      SpAsics=res.data.length;
+      this.setState({SpAsics})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Sport/Puma')
+    .then(res => {
+      SpPuma=res.data.length;
+      this.setState({SpPuma})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Sport/Salomon')
+    .then(res => {
+      SpSal=res.data.length;
+      this.setState({SpSal})
+    })
+
+    //************************************************** 
+    //Plage
+
+    axios.get('http://localhost:3000/shoes/categorie/Plage/Nike')
+    .then(res => {
+      PNike=res.data.length;
+      this.setState({PNike})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Plage/Fila')
+    .then(res => {
+      PFila=res.data.length;
+      this.setState({PFila})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Plage/Havaianas')
+    .then(res => {
+      PHav=res.data.length;
+      this.setState({PHav})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Plage/Ipanema')
+    .then(res => {
+      PIpa=res.data.length;
+      this.setState({PIpa})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Plage/Kappa')
+    .then(res => {
+      PKappa=res.data.length;
+      this.setState({PKappa})
+    })
+
+    //**************************************************
+    //Ville
+
+    axios.get('http://localhost:3000/shoes/categorie/Ville/Bexley')
+    .then(res => {
+      VBex=res.data.length;
+      this.setState({VBex})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Ville/Grenson')
+    .then(res => {
+      VGren=res.data.length;
+      this.setState({VGren})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Ville/Celio')
+    .then(res => {
+      VCel=res.data.length;
+      this.setState({VCel})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Ville/Chloe')
+    .then(res => {
+      VChloe=res.data.length;
+      this.setState({VChloe})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Ville/Berluti')
+    .then(res => {
+      VBerl=res.data.length;
+      this.setState({VBerl})
+    })
+
+    //**************************************************
+    //Bottines
+
+    axios.get('http://localhost:3000/shoes/categorie/Bottines/DrMartens')
+    .then(res => {
+      BDoc=res.data.length;
+      this.setState({BDoc})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Bottines/Ugg')
+    .then(res => {
+      BUgg=res.data.length;
+      this.setState({BUgg})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Bottines/MoonBoot')
+    .then(res => {
+      BMoon=res.data.length;
+      this.setState({BMoon})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Bottines/Timberland')
+    .then(res => {
+      BTimb=res.data.length;
+      this.setState({BTimb})
+    })
+
+    axios.get('http://localhost:3000/shoes/categorie/Bottines/Minnelli')
+    .then(res => {
+      BMin=res.data.length;
+      this.setState({BMin})
+    })
+
+    //**************************************************
+
  }
  
-
-
 
   render() {
     const dataSneakers = [
@@ -128,7 +296,7 @@ export default class Example extends PureComponent {
         Marque: 'Vans', NB_Ventes: SnVans,
       },
       {
-        Marque: 'Puma', NB_Ventes: SnPumas,
+        Marque: 'Puma', NB_Ventes: SnPuma,
       },
       {
         Marque: 'Conver.', NB_Ventes: SnConv,
@@ -138,70 +306,70 @@ export default class Example extends PureComponent {
 
     const dataSport = [
       {
-        Marque: 'Nike', NB_Ventes: 4000,
+        Marque: 'Nike', NB_Ventes: SpNike,
       },
       {
-        Marque: 'Adidas', NB_Ventes: 3000, 
+        Marque: 'Adidas', NB_Ventes: SpAd, 
       },
       {
-        Marque: 'Asics', NB_Ventes: 2000,
+        Marque: 'Asics', NB_Ventes: SpAsics,
       },
       {
-        Marque: 'Puma', NB_Ventes: 2780,
+        Marque: 'Puma', NB_Ventes: SpPuma,
       },
       {
-        Marque: 'Salom.', NB_Ventes: 1890,
+        Marque: 'Salom.', NB_Ventes: SpSal,
       },
     ];
     const dataPlage = [
       {
-        Marque: 'Nike', NB_Ventes: 4000,
+        Marque: 'Nike', NB_Ventes: PNike,
       },
       {
-        Marque: 'Fila', NB_Ventes: 3000, 
+        Marque: 'Fila', NB_Ventes: PFila, 
       },
       {
-        Marque: 'Havai.', NB_Ventes: 2000,
+        Marque: 'Havai.', NB_Ventes: PHav,
       },
       {
-        Marque: 'Ipa.', NB_Ventes: 2780,
+        Marque: 'Ipa.', NB_Ventes: PIpa,
       },
       {
-        Marque: 'Kappa', NB_Ventes: 1890,
+        Marque: 'Kappa', NB_Ventes: PKappa,
       },
     ];
     const dataVille = [
       {
-        Marque: 'Bexl.', NB_Ventes: 4000,
+        Marque: 'Bexl.', NB_Ventes: VBex,
       },
       {
-        Marque: 'Grenson', NB_Ventes: 3000, 
+        Marque: 'Grenson', NB_Ventes: VGren, 
       },
       {
-        Marque: 'Celio', NB_Ventes: 2000,
+        Marque: 'Celio', NB_Ventes: VCel,
       },
       {
-        Marque: 'Azzaro', NB_Ventes: 2780,
+        Marque: 'Chloe', NB_Ventes: VChloe,
       },
       {
-        Marque: 'Berluti', NB_Ventes: 1890,
+        Marque: 'Berluti', NB_Ventes: VBerl,
       },
     ];
     const dataBottes = [
       {
-        Marque: 'Doc Mr.', NB_Ventes: 4000,
+        Marque: 'Doc Mr.', NB_Ventes: BDoc,
       },
       {
-        Marque: 'Ugg', NB_Ventes: 3000, 
+        Marque: 'Ugg', NB_Ventes: BUgg, 
       },
       {
-        Marque: 'Moon.', NB_Ventes: 2000,
+        Marque: 'Moon.', NB_Ventes: BMoon,
       },
       {
-        Marque: 'Timb.', NB_Ventes: 2780,
+        Marque: 'Timb.', NB_Ventes: BTimb,
       },
       {
-        Marque: 'Minneli', NB_Ventes: 1890,
+        Marque: 'Minneli', NB_Ventes: BMin,
       },
     ];
     var datatest = [
